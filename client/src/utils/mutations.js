60919b3b348d61aa1d98d9979a365ee5aaa2c935
Alexis Lopez <lopez111alex@gmail.com>
2024-01-24 01:27:46 -0800

    made mutations for utils in the client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($email: String!, $password: String!, $firstName: String!, $lastName: String!) {
+    addUser(email: $email, password: $password, firstName: $firstName, lastName: $lastName) {
+      token
+      user {
+        _id
+        email
+        password
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+
+
