2726fad77fae4b14aa01517c5fc4e14273a6debb
Alexis Lopez <lopez111alex@gmail.com>
2024-01-24 01:25:00 -0800

    made basic main jsx in src folder in the client side

---
+++ client/src/main.jsx
@@ -0,0 +1,31 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+import Home from './pages/HomePage.jsx';
+import Login from './pages/LoginPage.jsx';
+import Signup from './pages/Signup.jsx';
+import ContactUs from './pages/ContactUs';
+import Error from './pages/Error.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true, 
+        element: <Home />
+      },
+      { path: '/login', element: <Login /> },
+      { path: '/signup', element: <Signup />},
+      { path: '/contact-us', element: <ContactUs /> },
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />)
