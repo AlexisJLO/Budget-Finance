0e85626eb9efb941498ea92ad7423c6fd555f933
Alexis Lopez <lopez111alex@gmail.com>
2024-01-24 01:26:21 -0800

    made basic App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,55 @@
+
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+    useQuery,
+    gql
+  } from '@apollo/client';
+  import { setContext } from '@apollo/client/link/context';
+  import { Outlet } from 'react-router-dom';
+  
+  import Header from './components/Header';
+  import Footer from './components/Footer';
+  import './App.css'
+  import { ThemeProvider } from './contexts/ThemeContext';
+  
+  const httpLink = createHttpLink({
+    uri: '/graphql',
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+  
+  
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+  
+  
+  function App() {
+    return (
+      <ApolloProvider client={client}>
+        <ThemeProvider>
+            <div className='App'>
+              <TopLevelHeader />
+              <Header />
+              <Outlet />
+              <Footer />
+            </div>
+        </ThemeProvider>
+      </ApolloProvider>
+    );
+  }
+  
+  export default App;
+  
